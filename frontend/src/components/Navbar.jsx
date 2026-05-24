74fe82a5515c2cf658c418d483747306e845a845
virajjadhav439 <virajjadhav439@gmail.com>
2026-05-24 12:41:58 +0530

    Improved landing page completed-Navbar,Heor-Section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import '../css/Navbar.css'
 const Navbar = () => {
   return (
     <>
     <nav className='navbar'>
     <h2 className='logo'>TBloger</h2>
 
-    <ul>
-        <li>
+    <ul className='navbar-options'>
+        <li className='navbar-home'>
         <Link to="/">Home</Link>
         </li>
 
-        <li>
+        <li className='navbar-login'>
             <Link to="/login">Login</Link>
         </li>
 
-        <li>
-            <Link to="/signup">Sign Up</Link>
+        <li className='navbar-signup'>
+            <Link to="/signup">Signup</Link>
         </li>
     </ul>
 
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import '../css/Home.css'
 const Home = () => {
   return (
     <>
       <Navbar/>
+      <section className='hero-section'>
+      <h1 className='hero-heading'>
+        Express Your Thoughts Beautifully
+      </h1>
+
+      <p className='hero-description'>
+        A modern glassmorphism blogging platform with immersive themes.
+      </p>
+
+      <button className='hero-action-button'>
+        Start Blogging
+        </button>
+      </section>
     </>
   )
 }
