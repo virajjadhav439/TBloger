8239b6ad21c8be98dcdcbb1069ffc9a6ba021303
virajjadhav439 <virajjadhav439@gmail.com>
2026-06-07 03:22:58 +0530

    ready for deployement v3

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../css/Home.css";
 import BlogCard from "../components/BlogCard";
